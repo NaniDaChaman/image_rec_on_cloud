57fc3e9f9acc4755acfc9b5a9fd6e4d95f11f989
Sparsh Amarnani <56154732+NaniDaChaman@users.noreply.github.com>
2024-09-26 12:41:32 -0500

    flask-api is set up perfectly

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask,jsonify,request,url_for,render_template
+from flask import Flask,jsonify,request,url_for,render_template,redirect
 import model as md
 
 
 
 app= Flask(__name__)
-@app.route("/get_pred",methods=["POST"])
+@app.route("/get_pred/<filename>")
 def get_pred(filename):
     prediction = md.model_pred(md.model_prob(filename))
     data={"Prediction" : prediction}
@@ -16,11 +16,13 @@ def hello_world():
 
 @app.route("/handle_form",methods=["POST"])
 def handle_form():
-    return "You did it"
-    ufile=request.files['image']
-    print(ufile.filename)
-    if ufile.filename!='':
-        return get_pred(ufile.filename)
+    #return "You did it"
+    ufile=request.form['image']
+    #print(ufile)
+   
+    #print(ufile.filename)
+    
+    return redirect(url_for("get_pred",filename=ufile))
 
 
 if __name__=="__main__":
